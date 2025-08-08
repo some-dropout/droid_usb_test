d7f84b2120eb808f4bf3821f57dacec333573952
dropout <223595116+some-dropout@users.noreply.github.com>
2025-08-08 21:33:04 +0900

    request permission
    Signed-off-by: dropout <223595116+some-dropout@users.noreply.github.com>

--- java/io/github/some_dropout/usb_test/MainActivity.java
+++ java/io/github/some_dropout/usb_test/MainActivity.java
@@ -3,9 +3,13 @@ package io.github.some_dropout.usb_test;
 import java.util.HashMap;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.ClipboardManager;
 import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
@@ -18,10 +22,52 @@ public class MainActivity extends Activity {
     System.loadLibrary("app_jni");
   }
 
+  private static final String ACTION_USB_PERM = "io.github.some_dropout.usb_test.USB_PERMISSION";
+
   private UsbManager mUsbMan;
   private EditText mOut;
+  private BroadcastReceiver mReceiver;
 
   private native int nativeTryOpen(String path);
+
+  private void initReceiver()
+  {
+    IntentFilter filter;
+
+    filter = new IntentFilter();
+    filter.addAction(ACTION_USB_PERM);
+
+    mReceiver = new BroadcastReceiver() {
+      @Override
+      public void onReceive(Context ctx, Intent intent)
+      {
+        String action;
+
+        action = intent.getAction();
+        if (action.equals(ACTION_USB_PERM))
+        {
+          boolean granted;
+
+          granted = intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false);
+          mOut.append(String.format("User %s permission request.\n", granted ? "granted" : "rejected"));
+          if (!granted)
+            return;
+        }
+      }
+    };
+
+    registerReceiver(mReceiver, filter, RECEIVER_NOT_EXPORTED);
+  }
+
+  private void requestPermission(UsbDevice dev)
+  {
+    PendingIntent intent;
+
+    intent = PendingIntent.getBroadcast(this, 0,
+                                        new Intent(ACTION_USB_PERM).setPackage(getPackageName()),
+                                        PendingIntent.FLAG_MUTABLE);
+    mUsbMan.requestPermission(dev, intent);
+  }
 
   @Override
   public boolean onKeyDown(int key, KeyEvent ev)
@@ -93,6 +139,12 @@ public class MainActivity extends Activity {
           mOut.append("More than one device connected, abort.\n");
           return;
         }
+
+        if (!firstHasPerm)
+        {
+          requestPermission(firstDev);
+          return;
+        }
       }
     });
 
@@ -114,5 +166,16 @@ public class MainActivity extends Activity {
         mOut.setText("");
       }
     });
+
+    initReceiver();
+  }
+
+  @Override
+  public void onDestroy()
+  {
+    unregisterReceiver(mReceiver);
+    mReceiver = null;
+
+    super.onDestroy();
   }
 }
